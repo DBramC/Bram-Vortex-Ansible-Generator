15b5ce8a2c0f5ad5a1f695e670b59f578178260e
DaBram <bramis04chris@gmail.com>
2026-04-09 17:17:24 +0300

    stack trace

--- src/main/java/com/christos_bramis/bram_vortex_ansible_generator/config/JwtAuthenticationFilter.java
+++ src/main/java/com/christos_bramis/bram_vortex_ansible_generator/config/JwtAuthenticationFilter.java
@@ -69,7 +69,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
             } catch (Exception e) {
                 System.out.println("⛔ Invalid Token in Ansible Gen: " + e.getMessage());
-                e.printStackTrace();
+                System.out.println(token);
             }
         }
         filterChain.doFilter(request, response);
